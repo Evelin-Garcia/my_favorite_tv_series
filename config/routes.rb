38f9f30f49c7db7bece444420c021decef0526a3
Evelin Garcia <e.garcia.santander@gmail.com>
2022-05-10 21:33:21 -0400

    commit clase 10/5

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
   get '/tv_shows', to: 'tv_shows#index'
+  get '/welcome', to: 'pages#welcome'
+  root 'pages#welcome'
 
-  root 'tv_shows#index'
 
 end
