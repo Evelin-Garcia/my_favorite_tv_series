c095c3fb960aaba914a04e56abd9f346279a8d34
Evelin Garcia <e.garcia.santander@gmail.com>
2022-05-19 21:17:57 -0400

    19/05, clase 11 scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  resources :tv_shows
   get '/tv_shows', to: 'tv_shows#index'
   get '/welcome', to: 'pages#welcome'
   post '/tv_shows', to: 'tv_shows#create'
